b8b2b6d3a32bf0a9c97258a6f9dc81b8592ad11c
preservance717 <GL717@outlook.com>
2016-05-19 18:12:09 +0800

    update

--- src/collect_all_even.js
+++ src/collect_all_even.js
@@ -1,7 +1,15 @@
 'use strict';
 
 function collect_all_even(collection) {
-  //在这里写入代码
+    var result = [];
+
+    collection.forEach(function (element) {
+        if (element % 2 === 0) {
+            result.push(element);
+        }
+    });
+
+    return result;
 }
 
 module.exports = collect_all_even;
